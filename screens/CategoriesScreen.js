44e60b24f69f0b659a81f1b1259584da5475b5ee
Spearek <30389214+Spearek@users.noreply.github.com>
2020-04-03 17:24:53 +0200

    Grid tile moved to own component

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
-import Colors from '../constants/Colors'
+import Colors from '../constants/Colors';
+import CategoryGridTile from '../components/CategoryGridTile'
 
 
 
 const CategoriesScreen = props => {
 
     const renderGridItem = (itemData) => {
-        return (
-            <TouchableOpacity style={styles.gridItem}
-                onPress={() => {
-                    props.navigation.navigate({routeName: 'CategoryMeals', params: {
+        return <CategoryGridTile
+            title={itemData.item.title}
+            color={itemData.item.color}
+            onSelect={() => {
+                props.navigation.navigate({
+                    routeName: 'CategoryMeals', params: {
                         categoryId: itemData.item.id
-                    }})
-                }}>
-                <View>
-                    <Text>{itemData.item.title}</Text>
-                </View>
-            </TouchableOpacity>
-        )
+                    }
+                })
+            }}
+        />
     }
 
     return (
@@ -32,12 +32,12 @@ const CategoriesScreen = props => {
     );
 };
 
-CategoriesScreen.navigationOptions = { 
+CategoriesScreen.navigationOptions = {
     headerTitle: 'Meal Categories',
     headerStyle: {
         backgroundColor: Colors.primaryColor
     },
-    headerTintColor: 'white' 
+    headerTintColor: 'white'
 }
 
 const styles = StyleSheet.create({
@@ -45,11 +45,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    },
-    gridItem: {
-        flex: 1,
-        margin: 15,
-        height: 150
     }
 });
 
