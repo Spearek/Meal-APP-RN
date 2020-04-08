f306e855f6eba76b6d40ba308620fd87b09a69f4
Spearek <30389214+Spearek@users.noreply.github.com>
2020-04-08 21:11:43 +0200

    dummy data displayed instead of empty component

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {MEALS} from '../data/dummy-data';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
+
+
+import MealList from '../components/MealList';
 
 const FavoritesScreen = props => {
+    const favMeals = MEALS.filter(meal => meal.id ==='m1' || meal.id ==='m2');
 
     return(
-        <View style = {styles.screen}>
-            <Text>The Favorites Screen</Text>
-        </View>
+        <MealList listData={favMeals} navigation={props.navigation}/>
     );
 };
 
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+FavoritesScreen.navigationOptions = (navData) =>{
+    return {
+        headerTitle:'Your Favorites',
+        headerLeft: () => {
+            return (
+                <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                    <Item title="Menu" iconName="ios-menu" onPress={() => { navData.navigation.toggleDrawer() }} />
+                </HeaderButtons>
+            )
+        }
     }
-});
+    
+} 
 
 export default FavoritesScreen;
